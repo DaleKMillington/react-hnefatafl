cbdc5613d19955a7a728f592e0554e9015b06a6f
Dale Millington <dkmillington@tutanota.com>
2023-08-28 18:23:34 +0100

    Placeholder icons and row layout in place

--- src/App.tsx
+++ src/App.tsx
@@ -8,9 +8,34 @@ import './sass/index.scss';
 const App = () => {
     return (
         <>
-            <div className="header-row">
-                <a href="#" className="header-link">HNEFATAFL</a>
-                <a href="#" className="header-link">MENU</a>
+            <div className="content-wrapper">
+
+                <div className="header-row">
+                    <a href="#" className="header-link">HNEFATAFL</a>
+                    <a href="#" className="header-link">MENU</a>
+                </div>
+
+                <div className="action-row">
+                    <div>RULES</div>
+                    <div>UNDO</div>
+                    <div>RESTART</div>
+                    <div>TURN 19</div>
+                </div>
+
+                <div className="main-row">
+                    <div className="main-icon">
+                        <div className="main-icon__icon main-icon__icon--left">ᛟ</div>
+                    </div>
+                    <div>TEST</div>
+                    <div className="main-icon">
+                        <div className="main-icon__icon main-icon__icon--right">ᚾ</div>
+                    </div>
+                </div>
+
+                <div className="footer-row">
+                    <div>ADVERT BANNER</div>
+                </div>
+
             </div>
         </>
     )
