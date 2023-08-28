8d96277368612c5028e94fcc1c1b51abd4e2ae7c
Dale Millington <dkmillington@tutanota.com>
2023-08-28 13:32:49 +0100

    Header banner provisionally setup

--- src/App.tsx
+++ src/App.tsx
@@ -6,11 +6,14 @@
 import './sass/index.scss';
 
 const App = () => {
-  return (
-    <>
-        <h1>Place Holder</h1>
-    </>
-  )
+    return (
+        <>
+            <nav className="header-banner">
+                <div className="header-banner__text"><span>HNEFATAFL</span></div>
+                <p className="header-banner__text">MENU</p>
+            </nav>
+        </>
+    )
 }
 
 export default App;
